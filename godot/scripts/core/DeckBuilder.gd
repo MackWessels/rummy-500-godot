a8bda238a6f4003a03e295fd600dde6614a51557
MackWessels <mackwessels@gmail.com>
2026-01-21 17:31:37 -0500

    Update DeckBuilder.gd

--- godot/scripts/core/DeckBuilder.gd
+++ godot/scripts/core/DeckBuilder.gd
@@ -1,11 +1,25 @@
-extends Node
 
+extends RefCounted
+class_name DeckBuilder
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SUITS = ["S","H","D","C"]
+const RANKS = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
+static func build_shoe(num_decks: int, registry: CardRegistry) -> Array:
+	registry.clear()
+	
+	var shoe: Array = []
+	var serial = 0
+	
+	for deck in range(num_decks):
+		for suit in SUITS:
+			for rank in RANKS:
+				serial += 1
+				var card_id = "D%s-%s%s-%04d" % [deck, suit, str(rank), serial]
+				registry.add_card(card_id, deck, suit, rank)
+				shoe.append(card_id)
+	
+	return shoe
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+static func shuffle_in_place(arr: Array) -> void:
+	arr.shuffle()
