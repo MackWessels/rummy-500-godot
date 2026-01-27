faee963ffc8ae35bfe751ea10b22677391263fd8
MackWessels <mackwessels@gmail.com>
2026-01-26 19:27:55 -0500

    start for meld script

---
+++ godot/RulesConfig.gd
@@ -0,0 +1,7 @@
+extends RefCounted
+class_name RulesConfig
+
+const STOCK_EMPTY_SHUFFLE_EXCEPT_TOP := "RESHUFFLE_EXCEPT_TOP" 
+const STOCK_EMPTY_END_HAND := "END_HAND"                   
+
+var stock_empty_policy: String = STOCK_EMPTY_SHUFFLE_EXCEPT_TOP
