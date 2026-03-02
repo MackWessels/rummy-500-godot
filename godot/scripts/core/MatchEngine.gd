f38acdaf0867c29c5386d8d8b94fbdbecde20a90
MackWessels <mackwessels@gmail.com>
2026-03-02 14:56:46 -0500

    Update MatchEngine.gd

--- godot/scripts/core/MatchEngine.gd
+++ godot/scripts/core/MatchEngine.gd
@@ -7,6 +7,8 @@ var match_state: MatchState
 var state: GameState
 var rules: RulesConfig
 
+var state_version: int = 0
+
 func _init(
 	num_players: int,
 	target_score: int = 500,
@@ -22,22 +24,40 @@ func _init(
 	
 	match_state = MatchState.new(num_players, target_score, dealer)
 	state = match_state.start_new_hand(registry)
+	
+	state_version = 1
 
 func apply(player: int, action: Dictionary) -> Dictionary:
-	# Returns the ActionProcessor result + some match info.
-	var res = ap.apply(state, player, action)
+	var res: Dictionary = ap.apply(state, player, action)
+	
+	# Increment version only on successful mutations
+	if bool(res.get("ok", false)):
+		state_version += 1
 	
 	# If the hand ended during this action, roll match forward.
 	if state.hand_over and state.hand_scored:
-		var fin = match_state.finalize_hand(state)
-		res["match_finalize_ok"] = fin.ok
+		var fin: Dictionary = match_state.finalize_hand(state)
+		
+		res["match_finalize_ok"] = bool(fin.get("ok", false))
 		res["match_totals"] = match_state.total_scores.duplicate()
 		res["match_winner"] = match_state.winner
 		
 		if match_state.winner == -1:
 			state = match_state.start_new_hand(registry)
 			res["new_hand_started"] = true
+			state_version += 1
 		else:
 			res["new_hand_started"] = false
 	
+	# Always attach a public snapshot for UI/network
+	res["state_version"] = state_version
+	# Hotseat: reveal_all=true. Later for multiplayer clients, switch to false.
+	res["state_public"] = state.to_public(player, true)
+	
 	return res
+
+func get_state_public(player: int, reveal_all: bool = true) -> Dictionary:
+	return {
+		"state_version": state_version,
+		"state_public": state.to_public(player, reveal_all)
+	}
